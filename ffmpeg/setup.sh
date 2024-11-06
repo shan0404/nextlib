74b7c86204804554ff798b0008e1ff71d683e1c1
okjack <okjackcaptain@gmail.com>
2024-11-06 13:59:54 +0800

    av3ad

--- ffmpeg/setup.sh
+++ ffmpeg/setup.sh
@@ -72,7 +72,9 @@ function downloadFfmpeg() {
   pushd $SOURCES_DIR
   git clone https://github.com/okcaptain/okjack-ffmpeg.git ffmpeg-${FFMPEG_VERSION}
   cd ffmpeg-${FFMPEG_VERSION}
-  git checkout okjack${FFMPEG_VERSION}-nextlib
+  git checkout okjack${FFMPEG_VERSION}-nextlib -b okjack
+  ls -al ./
+  cat ./libavcodec/av3adec.c
   popd
 }
 
