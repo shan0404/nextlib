960c53c8cc8d0705b9339164a08829d9f0558a05
okjack <okjackcaptain@gmail.com>
2024-11-07 14:20:31 +0800

    av3ad

--- ffmpeg/setup.sh
+++ ffmpeg/setup.sh
@@ -220,6 +220,7 @@ function buildFfmpeg() {
       --extra-ldflags="-ldl $DEP_LD_FLAGS" \
       --pkg-config="$(which pkg-config)" \
       --target-os=android \
+      --enable-gpl \
       --enable-shared \
       --disable-static \
       --disable-doc \
